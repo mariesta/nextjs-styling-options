ffe8a2007adcca8cb9391f824de026c634e8d8a5
Marie Starck <marie.compsci14@gmail.com>
2022-02-16 17:33:13 +0100

    With CSS modules

---
+++ with-css-modules/components/Title.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Title.module.css'
+
+export default function Title() {
+  return (
+    <h1 className={styles.title}>
+      Welcome to <a href="https://nextjs.org">Next.js!</a>
+    </h1>
+  )
+}
--- with-css-modules/pages/index.js
+++ with-css-modules/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+import Footer from '../components/Footer';
+import Title from '../components/Title';
 
 export default function Home() {
   return (
@@ -12,9 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Title />
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -51,19 +51,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
