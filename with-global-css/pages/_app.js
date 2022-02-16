0ed841194eee443eab677d1eb99e7428f13c0023
Marie Starck <marie.compsci14@gmail.com>
2022-02-16 17:13:33 +0100

    example with Global CSS

---
+++ with-global-css/pages/_app.js
@@ -0,0 +1,8 @@
+//In _app.js, import your global stylesheets
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
