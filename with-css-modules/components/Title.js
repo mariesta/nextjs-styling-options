a0690bb21a2356727a6bd661edd8d711dd54a926
Marie Starck <marie.compsci14@gmail.com>
2022-02-24 15:30:55 +0100

    update styling with css modules

--- with-css-modules/components/Title.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Title.module.css'
-
-export default function Title() {
-  return (
-    <h1 className={styles.title}>
-      Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </h1>
-  )
-}
